0c7c766d587027e4bffe657fc75b5a06adf51f9e
Parker Stenbo <24378ps@hvhs.school.nz>
2026-03-30 09:56:26 +1300

    I tried adding a slideshow and broke it >:(

---
+++ Articles/Javascript.js
@@ -0,0 +1,23 @@
+//I got this script from w3 shcools, but I changed it to match the code I have in the html/css as I didn't include some of the elements refrenced here.
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+}
